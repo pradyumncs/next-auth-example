2c5b3c4270eac06174a655d5ded80da453e2decd
pradyumnsuper <pradyumnsuper@gmail.com>
2024-02-24 01:25:23 +0530

    s

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
-import CustomLink from "@/components/custom-link"
-import packageJSON from "../package.json"
+
 import Link from 'next/link';
-import data from '../data.json';    
 import Carduno from "@/components/carduno";
 export default function Index() {
   return (
